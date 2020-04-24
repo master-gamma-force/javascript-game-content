d2aaae2f5b281e0db77e15f344098c73e9f4d153
Humberto Lopez <humbertolopez954@gmail.com>
2020-04-23 19:36:31 -0500

    Add test

---
+++ test/filter.js
@@ -0,0 +1,26 @@
+let leonidas = {
+  name: "Leondias",
+  skill:  "JavaScript"
+}
+let nicole = {
+  name: "Nicole",
+  skill: "Python"
+}
+let richard = {
+  name: "Richard",
+  skill: "Python"
+}
+let angela = {
+  name: "Angela",
+  skill: "JavaScript"
+}
+
+const employees = [];
+
+const  knowPython = () => {
+
+}
+
+const sabePython = employees.method(function);
+
+console.log(sabePython);
---
+++ test/test/test-filter.js
@@ -0,0 +1,42 @@
+const chai = require("chai");
+const assert = chai.assert;
+
+
+let leonidas = {
+  name: "Leondias",
+  skill: "JavaScript",
+};
+let nicole = {
+  name: "Nicole",
+  skill: "Python",
+};
+let richard = {
+  name: "Richard",
+  skill: "Python",
+};
+let angela = {
+  name: "Angela",
+  skill: "JavaScript",
+};
+
+const employees = [leonidas, nicole, richard, angela];
+
+const knowPython = (employee) => {
+  return employee.skill === "Python";
+};
+
+const sabePython = employees.filter(knowPython);
+
+assert.deepEqual(sabePython, [
+  { name: "Nicole", skill: "Python" },
+  { name: "Richard", skill: "Python" },
+]);
+
+describe("Test Filter", function () {
+  it("Testeando", function () {
+    assert.deepEqual(sabePython, [
+      { name: "Nicole", skill: "Python" },
+      { name: "Richard", skill: "Python" },
+    ]);
+  });
+});
